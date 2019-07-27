ca628e0366a9c3fd99f8f0500fd085e34a627342
Jon Mockbee <mockbee1000@gmail.com>
2019-07-26 20:45:54 -0400

    simple switch with cases

--- src/com/JonMockbee/Main.java
+++ src/com/JonMockbee/Main.java
@@ -3,6 +3,33 @@ package com.JonMockbee;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        int value = 1;
+//        if (value == 1) {
+//            System.out.println("value was 1");
+//        } else if (value == 2) {
+//            System.out.println("value was 2");
+//        } else {
+//
+//            System.out.println(was not 1or 2);
+//
+//        }
+        int switchValue = 4;
+
+        switch (switchValue){
+            case 1:
+                System.out.println("Value was ");
+                break;
+
+            case 3: case 4: case 5:
+                System.out.println("was a 3 ,4 or 5");
+                break;
+            default:
+                System.out.println("was not one or two");
+                break;
+        }
+        /// more code here after break;
+
     }
+
 }
+
